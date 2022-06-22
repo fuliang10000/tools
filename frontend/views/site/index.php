d3fbfae8428516331f7c3fdc7237577acdf29844
fuliang <fuliang19890809@163.com>
2022-06-22 22:17:50 +0800

    首页

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,47 +7,30 @@ $this->title = '工具盒';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>震惊！震惊！震惊！</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">这里的所有工具居然全部免费，需要用到的时候找不到了怎么办，赶紧收藏！</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://github.com/fuliang10000/tools" target="_blank">去白嫖代码，去点赞</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <h2>用户协议</h2>
+                <p>本网站为个人开发爱好者开发，只提供给个人使用，不存在任何商业用途或侵权等问题，若有任何问题请及时联系我。</p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                <h2>提建议，提需求</h2>
+                <p>以上工具是否能满足你的需要，是否还有待改进的地方，你是否还有想要的其他小工具；联系我，给我提需求，我为你免费开发。</p>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                <h2>联系我</h2>
+                <p>QQ：632249982</p>
+                <p>微信：fu200811</p>
+                <p>邮箱：fuliang19890809@163.com</p>
             </div>
         </div>
-
     </div>
 </div>
