757050cde26d7e328730a14e4a56b50655fe8ef5
fuliang <fuliang19890809@163.com>
2022-07-17 16:24:27 +0800

    提交

--- frontend/business/GrayImage.php
+++ frontend/business/GrayImage.php
@@ -33,7 +33,7 @@ class GrayImage extends Base
             $grayPath = $this->grayImg($form->file->tempName, $imgType);
             if (!$grayPath) throw new \Exception('生成图片失败');
 
-            $this->result = $grayPath;
+            $this->result = str_replace('https://', 'http://', $grayPath);
 
         } catch (\Exception $ex) {
             $this->code = 500;
